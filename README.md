6bb4a802b00031f5044276fb10fcd9441826ca0f
younghoon <digitcomgisul@gmail.com>
2021-03-22 10:38:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 프로젝트 설명
+
+### spring security 권한 DB 에서 관리 ###
+* hard coding하지 않고 메뉴에서 관리하는 방법에 대한 테스트 진행
---
+++ src/test/java/kr/digitcom/securitydb/SecuritydbApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.digitcom.securitydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
